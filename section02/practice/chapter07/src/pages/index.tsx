37550603f00c91f48fe779a77b9099ab3c31c042
gahyoenj <rkgus678@gmail.com>
2024-10-27 23:08:18 +0900

    chapter11까지 정리

--- section02/practice/chapter07/src/pages/index.tsx
+++ section02/practice/chapter07/src/pages/index.tsx
@@ -1,10 +1,33 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import style from "./index.module.css";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = () => {
+  //컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+
+  const data = "hello";
+
+  return {
+    props: {
+      data,
+    }, // 객체 타입이어야 객체 읽어와서 객체 안에 있는 props라는 property 값을 페이지에 전달해줌
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+  // 이 콘솔은 두번 찍힘 => 사전레더링을 위해서 서버에서 한번 브라우저에서 한번(hydration)에서 한번 총 두번 실행됨
+  //따라서 이 안에서도 window. 못함! => 서버에서 실행되므로
+
+  useEffect(() => {
+    console.log(window);
+  }, []); // useEffect 사용하면 마운트 됐을때 실행되므로 브라우저에서만 실행됨
+
   return (
     <div className={style.container}>
       <section>
